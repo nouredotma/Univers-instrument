296d2206c71894d3a01431f6deda2128132a755f
nordixdotma <noureddineelmhassani@gmail.com>
2026-03-08 02:33:20 +0000

    first commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
       {/* Background Image - Light overlay added */}
       <div className="absolute inset-0 w-full h-full z-0 overflow-hidden">
         <img
-          src="https://images.unsplash.com/photo-1602052577122-f73b9710adba?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="/5.png"
           alt="Univers Instrument Service"
           className="w-full h-full object-cover select-none"
         />
